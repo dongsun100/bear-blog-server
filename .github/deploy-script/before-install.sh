852ba670f7cd720647d0cd31931827a628853569
ds.choi <dongsun100@gmail.com>
2023-12-10 02:57:43 +0900

    ...

--- .github/deploy-script/before-install.sh
+++ .github/deploy-script/before-install.sh
@@ -4,7 +4,7 @@ echo "$(date '+%d/%m/%Y %H:%M:%S') [BeforeInstall]을 시작 합니다." >> /hom
 
 cd /home/ubuntu/code-deploy/
 tar cvf bear-blog-server-backup.tar --exclude bear-blog-server/node_modules bear-blog-server
-rm -rf bear-blog-server
-mkdir bear-blog-server
+# rm -rf bear-blog-server
+# mkdir bear-blog-server
 
 echo "$(date '+%d/%m/%Y %H:%M:%S') [BeforeInstall]을 종료 합니다." >> /home/ubuntu/code-deploy/deploy.log
