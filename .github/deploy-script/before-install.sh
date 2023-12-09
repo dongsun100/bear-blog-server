946e8c821f89d13223fc664b3d8a29127d6a2340
ds.choi <dongsun100@gmail.com>
2023-12-10 03:47:53 +0900

    ..

--- .github/deploy-script/before-install.sh
+++ .github/deploy-script/before-install.sh
@@ -10,7 +10,7 @@ export NVM_DIR="$HOME/.nvm"
 
 cd /home/ubuntu/code-deploy/
 tar cvf bear-blog-server-backup.tar --exclude bear-blog-server/node_modules bear-blog-server
-# rm -rf bear-blog-server
-# mkdir bear-blog-server
+rm -rf bear-blog-server
+mkdir bear-blog-server
 
 echo "$(date '+%d/%m/%Y %H:%M:%S') [BeforeInstall]을 종료 합니다." >> /home/ubuntu/code-deploy/deploy.log
