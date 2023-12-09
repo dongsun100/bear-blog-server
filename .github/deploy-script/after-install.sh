33a1f3525e38384cc3caa9ec9d1e212a2cecf59d
ds.choi <dongsun100@gmail.com>
2023-12-10 03:14:41 +0900

    .

--- .github/deploy-script/after-install.sh
+++ .github/deploy-script/after-install.sh
@@ -5,9 +5,9 @@ echo "$(date '+%d/%m/%Y %H:%M:%S') [AfterInstall]을 시작 합니다." >> /home
 
 cd /home/ubuntu/code-deploy/bear-blog-server
 
-sudo yarn install
+yarn install
 
-sudo npx prisma generate
-sudo npx prisma db pull
+npx prisma generate
+npx prisma db pull
 
 echo "$(date '+%d/%m/%Y %H:%M:%S') [AfterInstall]을 종료 합니다." >> /home/ubuntu/code-deploy/deploy.log
--- .github/deploy-script/application-start.sh
+++ .github/deploy-script/application-start.sh
@@ -4,6 +4,6 @@
 echo "$(date '+%d/%m/%Y %H:%M:%S') [ApplicationStart]을 시작 합니다." >> /home/ubuntu/code-deploy/deploy.log
 
 cd /home/ubuntu/code-deploy/bear-blog-server
-sudo yarn start:prod
+yarn start:prod
 
 echo "$(date '+%d/%m/%Y %H:%M:%S') [ApplicationStart]을 종료 합니다." >> /home/ubuntu/code-deploy/deploy.log
