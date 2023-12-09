bf8c579db7a321b2817d4a6e9ea5877af627f0c4
ds.choi <dongsun100@gmail.com>
2023-12-10 03:29:38 +0900

    .

--- .github/deploy-script/after-install.sh
+++ .github/deploy-script/after-install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export HOME="/home/ubuntu/"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 
 echo "$(date '+%d/%m/%Y %H:%M:%S') [AfterInstall]을 시작 합니다." >> /home/ubuntu/code-deploy/deploy.log
 
--- .github/deploy-script/before-install.sh
+++ .github/deploy-script/before-install.sh
@@ -2,6 +2,12 @@
 
 echo "$(date '+%d/%m/%Y %H:%M:%S') [BeforeInstall]을 시작 합니다." >> /home/ubuntu/code-deploy/deploy.log
 
+export HOME="/home/ubuntu/"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+
 cd /home/ubuntu/code-deploy/
 tar cvf bear-blog-server-backup.tar --exclude bear-blog-server/node_modules bear-blog-server
 # rm -rf bear-blog-server
