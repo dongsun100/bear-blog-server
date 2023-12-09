8eb752ceab856f0cd651ab7c845231cfb88858e7
ds.choi <dongsun100@gmail.com>
2023-12-10 03:44:17 +0900

    .

--- .github/deploy-script/application-stop.sh
+++ .github/deploy-script/application-stop.sh
@@ -5,12 +5,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-
-
 echo "$(date '+%d/%m/%Y %H:%M:%S') [ApplicationStop]을 시작 합니다." >> /home/ubuntu/code-deploy/deploy.log
-
-sudo npm install
-npm install pm2 -g
 
 cd /home/ubuntu/code-deploy/bear-blog-server
 pm2 kill
